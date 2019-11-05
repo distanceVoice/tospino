0433f023908e5ede68f026db0976de58625c3c17
lee <1433565248qq.com>
2019-11-05 22:12:24 +0800

    1.0.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import AppRouter from "./router"
+import './assets/css/test.less'
 class App extends React.Component {
     render(){
         return (
             <div style={{color:"#333"}} className="test test2">
-            <AppRouter />
+                <AppRouter />
             </div>
     )
     }
